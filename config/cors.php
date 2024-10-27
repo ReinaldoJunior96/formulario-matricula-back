1b9e42bae3bcae77446d7c2cffdb83afd9115b77
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2024-10-27 17:11:18 -0300

    ajustando back

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
