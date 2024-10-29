6a7d4f92a0938b206546710db9d7b4074bfb4b6b
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2024-10-29 19:04:46 -0300

    ajustando cors

--- config/cors.php
+++ config/cors.php
@@ -14,21 +14,13 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'paths' => ['api/*'],  // Define os endpoints que permitirão CORS, nesse caso, todos os endpoints da API.
+    'allowed_methods' => ['*'],  // Permite todos os métodos (GET, POST, etc).
+    'allowed_origins' => ['http://62.72.24.154:8081'],  // Permite o frontend React como origem.
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['*'],  // Permite todos os headers.
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 
 ];
