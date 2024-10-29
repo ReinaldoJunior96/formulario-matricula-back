5e9848970b29c7c1fa819e7822adcf9233bb457a
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2024-10-29 18:56:00 -0300

    ajustando cors

--- config/cors.php
+++ config/cors.php
@@ -1,19 +1,12 @@
 <?php
 
 return [
-    'paths' => ['*'],
-
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => ['*'],
-
+    'allowed_origins' => ['http://62.72.24.154:8081'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 ];
