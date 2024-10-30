9b2516758d29a047e01081f1e0165928c620518b
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2024-10-30 11:52:28 -0300

    ajustando url para mesma rede do docker

--- config/cors.php
+++ config/cors.php
@@ -17,8 +17,7 @@ return [
 
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://62.72.24.154:8081'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['http://62.72.24.154:8081'], // Ou '*' para permitir todas as origens
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
